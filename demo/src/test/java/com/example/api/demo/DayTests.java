d8ecff94e43ca84f7422d6f290c780b12b487d40
Jorrell Smith <sabatiel180@gmail.com>
2025-11-26 16:10:07 -0600

    go

---
+++ demo/src/test/java/com/example/api/demo/DayTests.java
@@ -0,0 +1,34 @@
+package com.example.api.demo;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayTests {
+
+    private static Day day;
+
+    @BeforeAll
+    static void setUpTests() {
+        // This method runs once before all tests in this class
+        day = Day();
+    }
+
+	@Test
+    void testState() {
+        day.setState("Bearish");
+        String expected = "Bearish";
+        String actual = day.getState();
+        assertEquals(expected, actual, "Get should be 'Bearish'");
+    }
+
+    @AfterAll
+    static void tearDownTests() {
+        // This method runs once after all tests in this class
+        day = null;
+        
+    }
+
+}
