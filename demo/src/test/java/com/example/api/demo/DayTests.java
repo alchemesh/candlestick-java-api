c7060d6ff63388f1285bc06c334915b1756680ca
Jorrell Smith <sabatiel180@gmail.com>
2025-11-26 19:58:02 -0600

    go

--- demo/src/test/java/com/example/api/demo/DayTests.java
+++ demo/src/test/java/com/example/api/demo/DayTests.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
 class DayTests {
 
     private static Day day;
--- demo/src/test/java/com/example/api/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/api/demo/DemoApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.api.demo;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 //import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class DemoApplicationTests {
 
 	@Test
