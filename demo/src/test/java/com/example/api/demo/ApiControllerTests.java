5e7185ca4468bd6c664809465b9a6e9ae1663af3
Jorrell Smith <sabatiel180@gmail.com>
2025-11-28 09:19:15 -0600

    go

---
+++ demo/src/test/java/com/example/api/demo/ApiControllerTests.java
@@ -0,0 +1,31 @@
+package com.example.api.demo;
+
+
+import org.junit.jupiter.api.Test;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+
+class ApiControllerTests {
+
+	@Test
+    void testAPI() {
+        HttpClient client = HttpClient.newHttpClient(); // Create an HTTP client
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.example.com/api/test")) // Set the target URI
+                .GET() // Specify the HTTP method (GET, POST, PUT, DELETE, etc.)
+                .build(); // Build the request
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()); // Send the request and handle the response body as a String
+
+
+        String expected = "Test Successful"
+        String actual = response.body();
+        assertEquals(expected, actual, "API call should be 'Test Succesful'");
+    }
+
+}
--- demo/src/test/java/com/example/api/demo/DayTests.java
+++ demo/src/test/java/com/example/api/demo/DayTests.java
@@ -25,6 +25,22 @@ class DayTests {
         assertEquals(expected, actual, "Get should be 'Bearish'");
     }
 
+    @Test
+    void testDate() {
+        day.setDate("2025-11-28");
+        String expected = "2025-11-28";
+        String actual = day.getDate();
+        assertEquals(expected, actual, "Get should be '2025-11-28'");
+    }
+
+    @Test
+    void testOpen() {
+        day.setOpen(248.35);
+        Double expected = 248.35;
+        Double actual = day.getOpen();
+        assertEquals(expected, actual, "Get should equal '248.35'");
+    }
+
     @AfterAll
     static void tearDownTests() {
         // This method runs once after all tests in this class
