ad5cb67c2a49184115da8f3e04318f733cd699ca
Jorrell Smith <sabatiel180@gmail.com>
2025-12-15 17:04:42 -0600

    go

--- demo/src/main/java/com/example/api/demo/DayDAO.java
+++ demo/src/main/java/com/example/api/demo/DayDAO.java
@@ -71,8 +71,9 @@ public class DayDAO {
                     ticker = rs.getString("Ticker");
                     name = rs.getString("Ticker_Name");
                     timestamp = rs.getString("Timestamp");
-                    stockDays.add(new Day(rs.getString("Day_Date"), rs.getDouble("Day_Open"), rs.getDouble("Day_Close"), rs.getDouble("Day_High"), rs.getDouble("Day_Low"), rs.getDouble("Day_Movement"), rs.getDouble("Day_Direction"), rs.getString("Day_State"), rs.getDouble("Day_PLowerWick"), rs.getDouble("Day_PBody"), rs.getDouble("Day_PUpperWick")));
-                    
+
+                    Day stockDay = new Day(rs.getString("Day_Date"), rs.getDouble("Day_Open"), rs.getDouble("Day_Close"), rs.getDouble("Day_High"), rs.getDouble("Day_Low"), rs.getDouble("Day_Movement"), rs.getDouble("Day_Direction"), rs.getString("Day_State"), rs.getDouble("Day_PLowerWick"), rs.getDouble("Day_PBody"), rs.getDouble("Day_PUpperWick")));
+                    stockDays.add(stockDay);
                 }
 
                 Event event = new Event(event_ID, ticker, timestamp);
